c0fb81ab4790140a21ab29cee235cb42991acaea
Samuel Adams <adamsam@Samuels-MBP.lan>
2016-12-24 15:38:57 -0600

    Added tab completion for buildings

--- completer.py
+++ completer.py
@@ -2,32 +2,30 @@ import readline
 import sys
 import os
 
-def list_contents(path):
-  if path.startswith(os.path.sep) or path.startswith('~'): # Absolute path 
-    basedir = os.path.dirname(os.path.expanduser(path))
-    contents = os.listdir(basedir)
-    contents = [os.path.join(basedir, d) for d in contents]
-  else: # Relative path
-    contents = os.listdir(os.curdir) 
-  contents = [d + os.path.sep if os.path.isdir(d) else d for d in contents]
-  return contents
+class Completer:
+  vocab_list = None
 
-def completer(text, state):
-  options = [x for x in list_contents(text) if x.startswith(os.path.expanduser(text))]
-  return options[state]
+  def __init__(self, vocab_list_in = None):
+    self.vocab_list = vocab_list_in
 
-def completer_init():
+  def list_from_path(self, path):
+    if path.startswith(os.path.sep) or path.startswith('~'): # Absolute path 
+      basedir = os.path.dirname(os.path.expanduser(path))
+      contents = os.listdir(basedir)
+      contents = [os.path.join(basedir, d) for d in contents]
+    else: # Relative path
+      contents = os.listdir(os.curdir) 
+    contents = [d + os.path.sep if os.path.isdir(d) else d for d in contents]
+    return contents
+
+  def completer(self, text, state):
+    if self.vocab_list:
+      options = [x for x in self.vocab_list if x.startswith(text)]
+      return options[state]
+    options = [x for x in self.list_from_path(text) if x.startswith(os.path.expanduser(text))]
+    return options[state]
+
+def readline_init():
   readline.parse_and_bind('tab: complete')
-  readline.set_completer(completer)
   readline.set_completer_delims(' \t\n`!@#$%^&*()-=+[{]}\\|;:\'",<>?')
-
-# def main():
-  # completer_init()
-  # while True:
-    # last_string = raw_input('? ')
-    # print 'Last input:', last_string
-
-# if __name__ == "__main__":
-  # main()
-
 
